f6379654b0be3f96d3e5fc2af6108ee7d82e69b2
foxane <65512187+foxane@users.noreply.github.com>
2024-05-31 18:02:23 +0700

    Update home to my liking

--- src/index/index.js
+++ src/index/index.js
@@ -2,3 +2,9 @@ import "./index.css";
 import homePage from "../home/home";
 
 document.querySelector("#content").appendChild(homePage());
+
+const menuBtn = document.getElementById("btn-menu");
+
+$(window).load(function () {
+  $("body").removeClass("preload");
+});
