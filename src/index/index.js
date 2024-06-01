487096e196c2d323f1f8b790211e4ef455c991cd
foxane <65512187+foxane@users.noreply.github.com>
2024-06-01 20:25:25 +0700

    Added pageload functions

--- src/index/index.js
+++ src/index/index.js
@@ -1,10 +1,40 @@
 import "./index.css";
 import homePage from "../home/home";
+import menuPage from "../menu/menu";
 
-document.querySelector("#content").appendChild(homePage());
+const content = document.querySelector("#content");
+// Remove preload class to prevent transition on page load
+(function () {
+  window.onload = function () {
+    const body = document.querySelector("body");
+    body.classList.remove("preload");
+    console.log("hi");
+  };
+})();
 
-const menuBtn = document.getElementById("btn-menu");
+const changePage = function (cases) {
+  let currPage = "";
+  if (cases === currPage) return;
+  content.innerHTML = "";
+  switch (cases) {
+    case "home":
+      content.appendChild(homePage());
+      currPage = cases;
+      break;
+    case "menu":
+      content.appendChild(menuPage());
+      currPage = cases;
+      break;
+  }
+};
 
-$(window).load(function () {
-  $("body").removeClass("preload");
-});
+const handler = (() => {
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      changePage(e.target.dataset.attr);
+    });
+  });
+})();
+
+changePage("home");
