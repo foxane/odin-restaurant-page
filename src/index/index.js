417941d74ed05caa3da4c3fe87eedd86a663b932
foxane <65512187+foxane@users.noreply.github.com>
2024-06-03 19:11:58 +0700

    Import about module

--- src/index/index.js
+++ src/index/index.js
@@ -2,6 +2,7 @@ import "./index.css";
 import homePage from "../home/home";
 import menuPage from "../menu/menu";
 import contactPage from "../contact/contact";
+import aboutPage from "../about/about";
 
 const content = document.querySelector("#content");
 // Remove preload class to prevent transition on page load
@@ -13,22 +14,19 @@ const content = document.querySelector("#content");
   };
 })();
 
-let bgPos = "right";
-const changeBg = () => {
-  document.querySelector(
-    "main"
-  ).style.cssText = `background-image:linear-gradient(to bottom ${bgPos},rgba(5, 5, 5, 0.808),rgb(0, 0, 0));`;
-  if (bgPos === "right") {
-    bgPos = "left";
-  } else {
-    bgPos = "right";
-  }
-};
-
 let currPage = "";
 const changePage = function (cases) {
   if (cases === currPage) return;
-  changeBg();
+
+  // Turn color for active page
+  document
+    .querySelector(`[data-attr="${cases}"]`)
+    .classList.add("nav__btn-active");
+  if (currPage !== "")
+    document
+      .querySelector(`[data-attr="${currPage}"]`)
+      .classList.remove("nav__btn-active");
+
   content.innerHTML = "";
   switch (cases) {
     case "home":
@@ -41,6 +39,10 @@ const changePage = function (cases) {
       break;
     case "contact":
       content.appendChild(contactPage());
+      currPage = cases;
+      break;
+    case "about":
+      content.appendChild(aboutPage());
       currPage = cases;
       break;
   }
